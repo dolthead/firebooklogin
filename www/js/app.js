b5abc06c5b5a952416ab16b9d71918c3fe96d060
Todd H <github@simplybest.com>
2016-06-02 22:48:56 -0600

    updated root ref

--- www/js/app.js
+++ www/js/app.js
@@ -6,7 +6,7 @@
             'ngAnimate',
             'ngCordova'
         ])
-        .constant('FirebaseUrl', 'https://firebooklogin.firebaseio.com/')
+        .constant('FirebaseUrl', 'https://<YOURFIREBASEHERE>.firebaseio.com/')
         .service('rootRef', ['FirebaseUrl', Firebase])
         .factory('Auth', Auth)
         .run(AppRun)
